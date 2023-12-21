e1eca2e52ad3a5f28d433e2b9a502b5f5edd8eae
DBulacio <daniel.bulacio@hotmail.com>
2023-12-21 01:17:10 -0300

    Adding material UI and some styling to user page

--- dbnexus_frontend/src/pages/UsersPage.js
+++ dbnexus_frontend/src/pages/UsersPage.js
@@ -1,53 +1,134 @@
-import React, {useState, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const UsersPage = () => {
-  const navigate = useNavigate()
-  let [users, setUsers] = useState([])
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState(null);
 
-  let getUsers = async () => {
-    let res = await fetch("/api/users/")
-    let data = await res.json()
+  const getUsers = async () => {
+    let res = await fetch("/api/users/");
+    let data = await res.json();
 
-    setUsers(data)
-    console.log('data', data)
-  }
+    setUsers(data);
+
+    let newRows = data.map(element => ({
+      id: element.id,
+      dni: element.dni,
+      phone: element.phone,
+      first_name: element.user.first_name,
+      last_name: element.user.last_name,
+    }));
+
+    setRows(newRows);
+  };
 
   useEffect(() => {
-    getUsers()
-  }, [])
+    getUsers();
+  }, []);
 
-  let deleteUser = async (id) => {
-    fetch(`/api/users/${id}/`, {
+  const deleteUser = async (id) => {
+    await fetch(`/api/users/${id}/`, {
       method: "DELETE",
       headers: {
         'Content-Type': 'application/json'
       }
-    })
-  }
+    });
+
+    setDeleteDialogOpen(false);
+
+    // refresh data after deletion
+    getUsers();
+  };
+
+  const handleEdit = (params) => {
+    const { id } = params.row;
+    navigate(`update/${id}`);
+  };
+
+  const handleDelete = (id) => {
+    setSelectedUserId(id);
+    setDeleteDialogOpen(true);
+  };
+
+  const handleCloseDeleteDialog = () => {
+    setDeleteDialogOpen(false);
+  };
+
+  const handleConfirmDelete = () => {
+    deleteUser(selectedUserId);
+  };
+
+  const CustomToolbar = () => (
+    <GridToolbarContainer>
+      <Button
+        color="primary"
+        onClick={() => navigate("add")}
+      >
+        Add User
+      </Button>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'first_name', headerName: 'First name', width: 150 },
+    { field: 'last_name', headerName: 'Last name', width: 150 },
+    { field: 'dni', headerName: 'DNI', type: 'number', width: 90 },
+    { field: 'phone', headerName: 'Phone', type: 'number', width: 130 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 160,
+      renderCell: (params) => (
+        <>
+          <Button onClick={() => handleEdit(params)}>Update</Button>
+          <Button onClick={() => handleDelete(params.row.id)} color="secondary">Delete</Button>
+        </>
+      ),
+    },
+  ];
 
   return (
-    <div>
-      <button onClick={() => navigate("add")}>(+) Add User</button>
+    <>
       <div>
         <h1>Listado de usuarios</h1>
-        {users.length > 0 ? (
-          <span>
-            {users.map((user, index) => (
-              <span key={index}>
-                <h2>{user.id}: {user.user.first_name} {user.user.last_name}, {user.dni}</h2>
-                <button onClick={() => navigate(`update/${user.id}`)}>(*) Update User</button>
-                <button onClick={() => deleteUser(user.id)}>(-) Delete User</button>
-              </span>
-            ))}
-          </span>
-        ) : (
-          <p>No hay usuarios para mostrar</p>
-        )}
-        
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+        />
       </div>
-    </div>
-  )
-}
+      <Dialog
+        open={deleteDialogOpen}
+        onClose={handleCloseDeleteDialog}
+      >
+        <DialogTitle>Delete User</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this user?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDeleteDialog}>Cancel</Button>
+          <Button onClick={handleConfirmDelete} color="secondary">Delete</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
 
-export default UsersPage
+export default UsersPage;
