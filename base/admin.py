4dbb8dba716e066eb26d65021019657e9edef01a
DBulacio <daniel.bulacio@hotmail.com>
2023-12-27 02:52:20 -0300

    Adding cities_light and Client and ClientData models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Client
+from .models import ClientData
+
+admin.site.register(Client)
+admin.site.register(ClientData)
--- base/models.py
+++ base/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# class Client(models.Model):
-#   name = models.CharField(max_length=50)
-#   dni  = models.PositiveIntegerField()
-#   cuil = models.PositiveBigIntegerField()
-#   tel  = models.CharField(max_length=50)
+class Client(models.Model):
+  name = models.CharField(max_length=50)
+  dni  = models.PositiveIntegerField()
+  cuil = models.PositiveBigIntegerField()
+  tel  = models.CharField(max_length=50)
 
-# class ClientData(models.Model):
-#   client = models.ForeignKey(Client, on_delete=models.CASCADE)
-#   dir    = models.CharField(max_length=100)
-#   num    = models.PositiveIntegerField()
-#   postal = models.CharField(max_length=20)
-#   country = 
+class ClientData(models.Model):
+  client = models.ForeignKey(Client, on_delete=models.CASCADE)
+  dir    = models.CharField(max_length=100)
+  num    = models.PositiveIntegerField()
+  postal = models.CharField(max_length=20)
+  country = models.ForeignKey('cities_light.Country', on_delete=models.SET_NULL, null=True, blank=True)
+  state = models.ForeignKey('cities_light.Region', on_delete=models.SET_NULL, null=True, blank=True)
