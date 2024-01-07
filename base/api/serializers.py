c48fd46dc2dd8a549362c06bce7bc4451bac0d5a
DBulacio <daniel.bulacio@hotmail.com>
2024-01-07 02:29:33 -0300

    adding serializers for all the new models

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
-from base.models import Client, Company, Product, Stock
+from base.models import Client, Company, Product, Stock, Balance, Service, Order, OrderHistory, OrderStatus
 from cities_light.models import Country, Region
 
 class UserSerializer(ModelSerializer):
@@ -29,6 +29,7 @@ class CompanySerializer(ModelSerializer):
     extra_kwargs = {
       'name' : {'required': False},
       'user' : {'required': False},
+      'is_active' : {'required': False},
     }
 
 class ClientSerializer(ModelSerializer):
@@ -54,7 +55,7 @@ class ProductSerializer(ModelSerializer):
     fields = '__all__'
     extra_kwargs = {
       'name':    {'required': False},
-      'active':  {'required': False},
+      'is_active':  {'required': False},
       'company': {'required': False},
     }
 
@@ -66,6 +67,61 @@ class StockSerializer(ModelSerializer):
       'product':  {'required': False},
       'date_in':  {'required': False},
       'date_out': {'required': False},
+    }
+
+class BalanceSerializer(ModelSerializer):
+  class Meta:
+    model = Balance
+    fields = '__all__'
+    extra_kwargs = {
+      'client': {'required': False},
+      'amount': {'required': False},
+      'transaction_type': {'required': False},
+      'timestamp': {'required': False},
+      'transaction_reference': {'required': False},
+      'currency': {'required': False},
+    }
+
+class ServiceSerializer(ModelSerializer):
+  class Meta:
+    model = Service
+    fields = '__all__'
+    extra_kwargs = {
+      'name': {'required': False},
+      'is_active': {'required': False},
+      'company': {'required': False},
+    }
+
+class OrderStatusSerializer(ModelSerializer):
+  class Meta:
+    model = OrderStatus
+    fields = '__all__'
+    extra_kwargs = {
+      'name': {'required': False},
+    }
+
+class OrderSerializer(ModelSerializer):
+  class Meta:
+    model = Order
+    fields = '__all__'
+    extra_kwargs = {
+      'client': {'required': False},
+      'service': {'required': False},
+      'cur_status': {'required': False},
+      'total_cost': {'required': False},
+      'company': {'required': False},
+      'date': {'required': False},
+    }
+
+class OrderHistorySerializer(ModelSerializer):
+  class Meta:
+    model = OrderHistory
+    fields = '__all__'
+    extra_kwargs = {
+      'order': {'required': False},
+      'status': {'required': False},
+      'company': {'required': False},
+      'date': {'required': False},
     }
 
 class CountrySerializer(ModelSerializer):
